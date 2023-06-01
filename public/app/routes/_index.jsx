13c081c3dd3081ef2ac70df15eb80b8ea9cce048
Jhonatan Jair Garcia Alfaro <joga47uk@gmail.com>
2023-06-01 09:07:09 -0500

    pushed date remix run

---
+++ public/app/routes/_index.jsx
@@ -0,0 +1,14 @@
+export const meta = () => {
+  return [
+    { title: "JgCode" },
+    { name: "description", content: "Mi portafolio personal" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <h1>Hola mundo</h1>      
+    </div>
+  );
+}

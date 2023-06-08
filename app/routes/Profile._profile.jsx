be43287744e340a7e8470f3e01a56b727a284c33
Jhonatan Jair Garcia Alfaro <joga47uk@gmail.com>
2023-06-08 17:23:22 -0500

    update portafolio

--- app/routes/Profile._profile.jsx
+++ app/routes/Profile._profile.jsx
@@ -9,23 +9,24 @@ export const meta = () => {
 const Profile = () => {
   const ProfilePersonal = {
     "Titulo": "Ingeniero de sistemas",
-    "Descripcion": "Soy un apasionado programador web fullstack con experiencia en desarrollo de aplicaciones utilizando tecnologías como PHP, JavaScript, React, MySQL y bases de datos no relacionales. He trabajado en diversos proyectos que abarcan desde el diseño y desarrollo de aplicaciones web interactivas hasta la implementación de bases de datos robustas y escalables. Mi enfoque principal es crear soluciones eficientes y centradas en el usuario, utilizando las mejores prácticas de desarrollo."
+    "Descripcion": "Soy un apasionado programador web fullstack con experiencia en desarrollo de aplicaciones utilizando tecnologías como PHP, JavaScript, React, MySQL y bases de datos no relacionales. He trabajado en diversos proyectos que abarcan desde el diseño y desarrollo de aplicaciones web interactivas hasta la implementación de bases de datos robustas y escalables. Mi enfoque principal es crear soluciones eficientes y centradas en el usuario, utilizando las mejores prácticas de desarrollo.",
+    "Img": "https://media.istockphoto.com/id/1279133399/es/foto/top-por-encima-de-la-vista-de-%C3%A1ngulo-alto-retrato-de-su-%C3%A9l-agradable-atractivo-enfocado.jpg?s=612x612&w=0&k=20&c=zut_6TclL8yZZedQD9n6t5suCuPI_D3y713IHceTW8Y="
   }
   return (
     <section className="Profile">
       <div className="content-profile">
         <div>
-          <img src="https://media.istockphoto.com/id/1279133399/es/foto/top-por-encima-de-la-vista-de-%C3%A1ngulo-alto-retrato-de-su-%C3%A9l-agradable-atractivo-enfocado.jpg?s=612x612&w=0&k=20&c=zut_6TclL8yZZedQD9n6t5suCuPI_D3y713IHceTW8Y=" alt="Prog" />
+          <img src={ProfilePersonal.Img} alt="Prog" />
         </div>
         <div>
           <h1>{ProfilePersonal.Titulo}</h1>
           <p>
-          {ProfilePersonal.Descripcion}
+            {ProfilePersonal.Descripcion}
           </p>
         </div>
       </div>
     </section>
-    )
+  )
 }
 
 export default Profile
